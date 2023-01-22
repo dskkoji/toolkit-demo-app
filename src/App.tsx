254dfa99c31a15fb6af2c70c1605f53f85a4ca3b
dskkoji <dkojima.2nd@gmail.com>
2023-01-23 07:40:01 +0900

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React, { useEffect } from 'react'
-
 import { useRoutes } from 'react-router-dom'
 import { routes } from './routes'
-import styles from './App.module.scss'
-import { useDispatch } from 'react-redux'
 import { AppDispatch } from './app/store'
-import { useAppDispatch, useAppSelector } from './app/hooks' 
+import { useAppDispatch } from './app/hooks' 
 import { onAuthStateChanged } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
-import { RootState } from './app/store'
-import { fetchProducts } from './features/product/productSlice'
-import { fetchUsers, fetchProductsInCart } from './features/user/userSlice'
-import Layout from './Layout'
+import { fetchUsers } from './features/user/userSlice'
 import { auth } from './firebase/index'
-
-
 
 const App: React.FC = () => {
   const navigate = useNavigate()
@@ -34,17 +26,6 @@ const App: React.FC = () => {
     return () => getData()
   }, [])
 
-
-//   return (
-//     <>
-//         <Header />
-//         <main className="c-main">
-//           {/* <ProductList /> */}
-//           <Router />
-//         </main> 
-//     </>
-//   )
-// }
  const router = useRoutes(routes)
   return (
     <>{router}</>
